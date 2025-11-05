9299e0d4baf6c7894b4337aa0b890e72a48c09a5
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-11-05 15:46:54 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const contactLink = document.getElementById('contact-link');
 const contactSection = document.getElementById('contact-section');
 const reviewsLink = document.getElementById('reviews-link');
 const reviewsSection = document.getElementById('reviews-section');
+const transitionOverlay = document.getElementById('transition-overlay');
 
 aboutLink.addEventListener('click', ()=>{
     goToAbout();
@@ -17,33 +18,58 @@ contactLink.addEventListener('click', ()=>{
     goToContact();
 })
 reviewsLink.addEventListener('click', ()=>{
-
+    goToReviews();
 })
 
 function goToAbout(){
     aboutSection.classList.remove('sticky');
+    transitionOverlay.classList.remove('hidden');
+    transitionOverlay.classList.add('transitioning');
     setTimeout(()=>{
         aboutSection.classList.add('sticky');
     }, 500);
+    setTimeout(()=>{
+        transitionOverlay.classList.add('hidden');
+        transitionOverlay.classList.remove('transitioning');
+    }, 2000);
 }
 
 function goToServices(){
     servicesSection.classList.remove('sticky');
+    transitionOverlay.classList.remove('hidden');
+    transitionOverlay.classList.add('transitioning');
     setTimeout(()=>{
         servicesSection.classList.add('sticky');
-    }, 10);
+    }, 500);
+    setTimeout(()=>{
+        transitionOverlay.classList.add('hidden');
+        transitionOverlay.classList.remove('transitioning');
+    }, 2000);
 }
 
 function goToContact(){
     contactSection.classList.remove('sticky');
+    transitionOverlay.classList.remove('hidden');
+    transitionOverlay.classList.add('transitioning');
     setTimeout(()=>{
         contactSection.classList.add('sticky');
-    }, 10);
+    }, 500);
+    setTimeout(()=>{
+        transitionOverlay.classList.add('hidden');
+        transitionOverlay.classList.remove('transitioning');
+    }, 2000);
 }
 
 function goToReviews(){
+    console.log('taking the hobits to review section');
     reviewsSection.classList.remove('sticky');
+    transitionOverlay.classList.remove('hidden');
+    transitionOverlay.classList.add('transitioning');
     setTimeout(()=>{
         reviewsSection.classList.add('sticky');
-    }, 10);
+    }, 500);
+    setTimeout(()=>{
+        transitionOverlay.classList.add('hidden');
+        transitionOverlay.classList.remove('transitioning');
+    }, 2000);
 }
