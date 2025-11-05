d3b7e3d03c7aa87fef5d1a27371f27f8ee2ea5be
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-11-05 13:47:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,16 +2,48 @@ const aboutLink = document.getElementById('about-link');
 const aboutSection = document.getElementById('about-section');
 const servicesLink = document.getElementById('services-link');
 const servicesSection = document.getElementById('services-section');
+const contactLink = document.getElementById('contact-link');
+const contactSection = document.getElementById('contact-section');
+const reviewsLink = document.getElementById('reviews-link');
+const reviewsSection = document.getElementById('reviews-section');
 
 aboutLink.addEventListener('click', ()=>{
+    goToAbout();
+});
+servicesLink.addEventListener('click', ()=>{
+    goToServices();
+})
+contactLink.addEventListener('click', ()=>{
+    goToContact();
+})
+reviewsLink.addEventListener('click', ()=>{
+
+})
+
+function goToAbout(){
     aboutSection.classList.remove('sticky');
     setTimeout(()=>{
         aboutSection.classList.add('sticky');
     }, 500);
-});
-servicesLink.addEventListener('click', ()=>{
+}
+
+function goToServices(){
     servicesSection.classList.remove('sticky');
     setTimeout(()=>{
         servicesSection.classList.add('sticky');
     }, 10);
-})
+}
+
+function goToContact(){
+    contactSection.classList.remove('sticky');
+    setTimeout(()=>{
+        contactSection.classList.add('sticky');
+    }, 10);
+}
+
+function goToReviews(){
+    reviewsSection.classList.remove('sticky');
+    setTimeout(()=>{
+        reviewsSection.classList.add('sticky');
+    }, 10);
+}
